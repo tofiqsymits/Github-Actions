1e050b3f76af7433669beacb259fbd20a46539e6
tOfi <tofi@kali>
2023-09-13 18:17:10 +0500

    Test Fail in G-A-2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,7 +8,7 @@ test('has title', async ({ page }) => {
 });
 
 test('get started link', async ({ page }) => {
-  test.fail();
+  //test.fail();
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
